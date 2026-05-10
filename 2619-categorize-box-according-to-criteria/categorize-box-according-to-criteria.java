295685a3cb8f1601ab04a0d7308fb2f076166838
iamrthombare <iamrthombare@gmail.com>
2026-05-10 16:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (92.73%) - LeetSync

---
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String categorizeBox(int length, int width, int height, int mass) {
+        boolean bulky = false;
+        boolean heavy = false;
+
+        long volume = (long) length * width * height;
+
+        if (length >= 10000 || width >= 10000 || height >= 10000 || volume >= 1000000000) {
+            bulky = true;
+        }
+
+        if (mass >= 100) {
+            heavy = true;
+        }
+
+        if (bulky && heavy) return "Both";
+        if (bulky) return "Bulky";
+        if (heavy) return "Heavy";
+        return "Neither";
+    }
+}
