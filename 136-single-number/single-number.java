187bce79011e8c0974e84f71228545bcaedd2f05
Rushikesh Thombare <iamrthombare@gmail.com>
2026-05-13 19:30:51 +0530

    Time: 531 ms (5.60%) | Memory: 47.1 MB (14.59%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        int n = arr.length;
+
+        for(int i  = 0 ; i< n ; i++){
+            int c =  0 ;
+            for(int j = 0 ; j<n ; j++){
+                if(arr[i]==arr[j]){
+                    c++;
+                }
+            }
+            if(c==1){
+                return arr[i];
+            }
+        }
+       return -1; 
+    }
+}
