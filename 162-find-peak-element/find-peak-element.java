f034030a9b69a5c024be3ef95c810529026f5216
Rushikesh Thombare <iamrthombare@gmail.com>
2026-05-15 09:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (18.38%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int l = 0, r = arr.length - 1;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                r = mid; 
+            } else {
+                l = mid + 1; 
+            }
+        }
+
+        return l;
+    }
+}
